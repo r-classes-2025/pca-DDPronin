e974c6c7c2c13dfbfe392fc42d872e2af7083b11
DDPronin <91835818+DDPronin@users.noreply.github.com>
2026-01-21 23:48:40 +0300

    6

--- assignment.R
+++ assignment.R
@@ -25,7 +25,8 @@ friends_tf <- friends_tokens |>
   count(speaker, word) |>
   group_by(speaker) |>
   mutate(tf = n / sum(n)) |>
-  slice_max(n, n = 500, with_ties = TRUE) |>
+  arrange(-n) |> 
+  slice_head(n = 500) |>
   ungroup() |>
   select(speaker, word, tf)
 
